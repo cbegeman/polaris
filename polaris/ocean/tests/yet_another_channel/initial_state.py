6035a1ce825293e5523d515bfeabfee8c85d4a32
Carolyn Begeman <cbegeman@lanl.gov>
2023-05-16 10:38:15 -0500

    Add mesh to initial_state

--- polaris/ocean/tests/yet_another_channel/initial_state.py
+++ polaris/ocean/tests/yet_another_channel/initial_state.py
@@ -1,4 +1,9 @@
+from mpas_tools.io import write_netcdf
+from mpas_tools.mesh.conversion import convert, cull
+from mpas_tools.planar_hex import make_planar_hex_mesh
+
 from polaris import Step
+from polaris.mesh.planar import compute_planar_hex_nx_ny
 
 
 class InitialState(Step):
@@ -25,3 +30,32 @@ class InitialState(Step):
         """
         super().__init__(test_case=test_case, name='initial_state')
         self.resolution = resolution
+
+    def run(self):
+        """
+        Run this step of the test case
+        """
+        logger = self.logger
+        config = self.config
+
+        section = config['yet_another_channel']
+        resolution = self.resolution
+
+        lx = section.getfloat('lx')
+        ly = section.getfloat('ly')
+
+        # these could be hard-coded as functions of specific supported
+        # resolutions but it is preferable to make them algorithmic like here
+        # for greater flexibility
+        nx, ny = compute_planar_hex_nx_ny(lx, ly, resolution)
+        dc = 1e3 * resolution
+
+        ds_mesh = make_planar_hex_mesh(nx=nx, ny=ny, dc=dc,
+                                       nonperiodic_x=False,
+                                       nonperiodic_y=True)
+        write_netcdf(ds_mesh, 'base_mesh.nc')
+
+        ds_mesh = cull(ds_mesh, logger=logger)
+        ds_mesh = convert(ds_mesh, graphInfoFileName='culled_graph.info',
+                          logger=logger)
+        write_netcdf(ds_mesh, 'culled_mesh.nc')
